401efce2fcbe5c63131a69dad0ded3694b6f511b
Alex Chen <alexchen@college.harvard.edu>
2015-03-28 13:37:02 -0400

    Add jQuery.min

---
+++ app/scripts/injector.js
@@ -0,0 +1,11 @@
+// Check for PMC full text link
+// Navigate to PMC full text page
+// Get and parse full text XML from OAI
+// https://developer.chrome.com/extensions/xhr need this to do cross-domain requests! e.g. from OAI
+// Get funding-source tag, if exists. This is the name of the organization that funded http://jats.nlm.nih.gov/publishing/tag-library/1.1d2/element/funding-source.html
+// Get funding-statement tag
+// Attempt to get conflict of interest statement
+// On the current Pubmed Abstract page:
+// If funding-source was NIH, have some indicator
+// Add some box to the top of the page with a hover over to display the funding-statement text
+// Add conflict of interest statement, highlighting authors’ names
